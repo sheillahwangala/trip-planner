bc970d4e5838f2763223b6064a00d52eede9efb2
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-12-05 21:34:41 +0300

    add methods to tripsController

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,2 +1,20 @@
 class TripsController < ApplicationController
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        trips = Trip.all
+        render json: trips
+    end
+
+    def show
+        trip = Trip.find(params[:id])
+        render json: trip
+    end
+
+    private
+
+    def render_not_found_response
+        render json: { error: "Hero not found" }, status: :not_found
+    end
+
 end
