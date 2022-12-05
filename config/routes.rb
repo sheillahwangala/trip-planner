4ab0fde055ff38e93457b3751bade56e253be229
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-12-05 21:48:21 +0300

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :stopovers
-  resources :destinations
-  resources :trips
+  resources :stopovers, only: [:show, create]
+  resources :destinations, only: [:index, :show]
+  resources :trips, only: [:index, :show]
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
