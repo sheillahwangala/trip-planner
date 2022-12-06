2d9ef617b9c41fd8a9fb2e76a0d3e09ac5d7b27a
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-12-06 11:48:58 +0300

    update sessions

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,20 +1,31 @@
 class TripsController < ApplicationController
-    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+    before_action :authorize
 
     def index
+        user = User.find_by(id: session[:user_id])
         trips = Trip.all
-        render json: trips
+        render json: trips, status: :created
     end
 
-    def show
-        trip = Trip.find(params[:id])
-        render json: trip
+    def create
+        @user = User.find_by(id: session[user_id])
+        trip = Trip.create(trip_params.merge({user_id: @user.id}))
+        if trip.valid?
+            render json: trip, status: :created
+        else
+            render json: { errors: trip.errors.full_messages }, status: :unprocessable_entity
+        end
     end
 
     private
 
-    def render_not_found_response
-        render json: { error: "Trip not found" }, status: :not_found
+    def authorize
+        return render json: { errors: ["Not authorized"] }, status: :unauthorized unless session.include? :user_id
     end
 
+    def trip_params
+        params.permit(:title, :start_date, :end_date)
+    end
+
+    
 end
