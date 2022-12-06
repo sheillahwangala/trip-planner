5faa90207c87adb3c1f0c53a721e78e96e15accc
sheillahwangala <sheillah.wangala@student.moringaschool.com>
2022-12-06 11:36:01 +0300

    update user model

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,4 +1,5 @@
 class Trip < ApplicationRecord
     has_many :stopovers
     has_many :destinations, through: :stopovers
+    belongs_to :user
 end
--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -1,3 +1,5 @@
 class UserSerializer < ActiveModel::Serializer
   attributes :id, :username, :password_digest, :about
+
+  has_many :trips
 end
